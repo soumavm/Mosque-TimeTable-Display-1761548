ca44a1eeb0e8dda80bd69d594699ff4dfeaa213c
GreenAvocado <greenavocado@protonmail.com>
2020-04-19 13:19:29 -0600

    fix: removed offset used to test date function

--- TimesScript.js
+++ TimesScript.js
@@ -5,18 +5,18 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+day;
 setInterval(checkDate, 60000);
 
 $(document).ready(function() {
-    $('#date').text(date);
+    $('#date').text("Today's date is: " + date);
 });
 
 function checkDate() {
     today = new Date();
-    day = today.getDate() + 1;
+    day = today.getDate();
     console.log("checking date: today");
 
     if(day != lastday) {
         date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+day;
         lastday = day;
-        $('#date').text(date);
+        $('#date').text("Today's date is: " + date);
         console.log("changing date");
     }
 }
