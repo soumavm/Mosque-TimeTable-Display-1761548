2c5ce73aefbb55d33a7b7c69ba8f59cc47de91fe
GreenAvocado <greenavocado@protonmail.com>
2020-04-19 13:14:32 -0600

    feat: added working date update function

--- TimesScript.js
+++ TimesScript.js
@@ -1,17 +1,23 @@
-setInterval(checkDate, 60000);
-var today = Date();
+var today = new Date();
 var day = today.getDate();
 var lastday = day;
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+day;
+setInterval(checkDate, 60000);
+
+$(document).ready(function() {
+    $('#date').text(date);
+});
 
 function checkDate() {
     today = new Date();
-    day = today.getDate();
+    day = today.getDate() + 1;
+    console.log("checking date: today");
 
     if(day != lastday) {
         date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+day;
         lastday = day;
+        $('#date').text(date);
+        console.log("changing date");
     }
-  }
 }
 
